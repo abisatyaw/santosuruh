fade65736df6b7dcf2353fa8adcda449d99da30e
abisatyaw <abisatya90@gmail.com>
2024-06-13 19:50:29 +0700

    fixed: chat feature

--- src/app_rtchat/views.py
+++ src/app_rtchat/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from  .models import ChatGroup
+from .forms import ChatmessageCreateForm
+@login_required
 # Create your views here.
 def chat_view(request):
-    return render(request, "chat.html")
+    chat_group = get_object_or_404(ChatGroup, group_name="pubchat")
+    chat_messages = chat_group.chat_messages.all()
+
+    form = ChatmessageCreateForm()
+    if request.method == 'POST':
+        form = ChatmessageCreateForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.group = chat_group
+            message.author = request.user 
+            message.save()
+            return redirect('chat')
+    return render(request, "chat.html", {'chat_messages': chat_messages, 'form': form})
